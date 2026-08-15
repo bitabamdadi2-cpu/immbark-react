d397a3e03b18b124c280e77a9d6cbe8b224aca6b
Sazgar <bitabamdadi2@gmail.com>
2026-08-15 14:32:26 +0330

    Initial commit: Immbark React project

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ImmbarkHero from './ImmbarkHero.jsx';
+import './ImmbarkHero.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ImmbarkHero />
+  </React.StrictMode>
+);
